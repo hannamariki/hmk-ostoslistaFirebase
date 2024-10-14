4fff85d7483f7b023ca2b6c8865f095826ab4831
Hanna-Mari Kielenniva <h.kielenniva@gmail.com>
2024-10-14 12:39:44 +0300

    tehtävän alustus
    lisätty firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0OHtKaYhdH7kdQlCcY9hORwLqB4Rbza8",
+  authDomain: "ostoslistafirebase-1e230.firebaseapp.com",
+  projectId: "ostoslistafirebase-1e230",
+  storageBucket: "ostoslistafirebase-1e230.appspot.com",
+  messagingSenderId: "91502160998",
+  appId: "1:91502160998:web:13d0f98d4ee352188291a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
